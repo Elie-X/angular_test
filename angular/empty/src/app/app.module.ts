f240a2422950b6846d3eb12fd607dfbc92f3d08f
Didier Tremblay <didier.tremblay@claurendeau.qc.ca>
2021-10-19 16:21:54 -0400

    More cleanup

--- angular/empty/src/app/app.module.ts
+++ angular/empty/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NewcomponentComponent } from './newcomponent/newcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NewcomponentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
