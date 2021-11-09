8849f9c6495566d2d5a79cf59467dc2cde1cb61e
Elie <eliebouchergendron@gmail.com>
2021-11-08 22:14:56 -0500

    First Commit

--- typescript/exercises/Calculatrice/calculator.start.js
+++ typescript/exercises/Calculatrice/calculator.start.js
@@ -1,63 +1,47 @@
 // Enum with all the operators that we want to support inside our code.
-enum Operator {
-    Add = "add",
-    Subtract = "subtract",
-    Multiply = "multiply",
-    Divide = "divide",
-    Modulo = "modulo",
-}
-
-// Type alias for a function that does a calculation on two numbers. 
-// Such a function will take two numbers as parameters, and return a single number.
-type Operation = (x: number, y: number) => number;
-
+var Operator;
+(function (Operator) {
+    Operator["Add"] = "add";
+    Operator["Subtract"] = "subtract";
+    Operator["Multiply"] = "multiply";
+    Operator["Divide"] = "divide";
+    Operator["Modulo"] = "modulo";
+})(Operator || (Operator = {}));
 // An (empty) array that can hold multiple tuples of the `[Operator, Operation]` type. 
 // This will be our dictionary, where we store all our methods.
-const operations: [Operator, Operation][] = [];
-
+const operations = [];
 // An add method, that satisfies the `Operation` type
-const add = function (first: number, second: number) {
+const add = function (first, second) {
     return first + second;
 };
-
 // Adding a tuple of the `Operator.Add` value and the `add` function to the `operations` array. 
 operations.push([Operator.Add, add]);
-
-const subtract = function (first: number, second: number) {
+const subtract = function (first, second) {
     return first - second;
 };
 operations.push([Operator.Subtract, subtract]);
-
-const multiply = function (first: number, second: number) {
+const multiply = function (first, second) {
     return first * second;
 };
 operations.push([Operator.Multiply, multiply]);
-
-const divide = function (first: number, second: number) {
+const divide = function (first, second) {
     return first / second;
 };
 operations.push([Operator.Divide, divide]);
-
-const modulo = function (first: number, second: number) {
+const modulo = function (first, second) {
     return first % second;
 };
-
 operations.push([Operator.Modulo, modulo]);
-
-const calculator = function (first: number, second: number, op: Operator) {
-    const tuple = operations.find(tpl => tpl[0] === op)!;
+const calculator = function (first, second, op) {
+    const tuple = operations.find(tpl => tpl[0] === op);
     const operation = tuple[1];
     const result = operation(first, second);
     return result;
-}
-
+};
 // A test run of the calculator
 console.log(calculator(4, 6, Operator.Add));
 console.log(calculator(13, 3, Operator.Subtract));
 console.log(calculator(2, 5, Operator.Multiply));
 console.log(calculator(70, 7, Operator.Divide));
 console.log(calculator(120, 11, Operator.Modulo));
-
-
 // Ajouter une operation Modulo
-
